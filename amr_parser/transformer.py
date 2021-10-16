d92772d175187497264e4ce2f57fdee1eb09f277
hankcs <jfservice@126.com>
2021-10-16 12:41:16 -0400

    Fix inplace bug fix https://github.com/emorynlp/levi-graph-amr-parser/issues/2

--- amr_parser/transformer.py
+++ amr_parser/transformer.py
@@ -131,8 +131,8 @@ class MultiheadAttention(nn.Module):
             q = self.in_proj_q(query)
             k = self.in_proj_k(key)
             v = self.in_proj_v(value)
+        q = q.clone()
         q *= self.scaling
-
 
         q = q.contiguous().view(tgt_len, bsz * self.num_heads, self.head_dim).transpose(0, 1)
         k = k.contiguous().view(-1, bsz * self.num_heads, self.head_dim).transpose(0, 1)
